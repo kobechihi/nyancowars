e43de1a8b7c1f0579f8082c0a1544495cc0f3968
佐藤ちひろ <kd1398216@st.kobedenshi.ac.jp>
2024-09-19 10:45:35 +0900

    Update zentaikeisan.py

--- nyanco_wars/zentaikeisan.py
+++ nyanco_wars/zentaikeisan.py
@@ -138,7 +138,7 @@ def main():
 
         st.session_state.opponent_team = pd.DataFrame(columns=['名前', '最高戦力', '属性'])
 
-    attributes = ["火", "水", "木", "火＆水", "火&木", "水&火", "水&木", "木&火", "木&水"]
+    attributes = ["火", "水", "木", "火&水", "火&木", "水&火", "水&木", "木&火", "木&水"]
 
     st.header("自チームメンバー登録")
 
